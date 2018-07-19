95ba40603d5d69d70e823587e9b48425650d0e26
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2018-07-19 17:02:59 +0100

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -47,7 +47,22 @@ app.get('/todos/:id', (req, res) => {
 
     res.status(200).send({todo});
   }).catch((e) => res.status(400).send());
-  
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id)
+    .then((todo) => {
+      if(!todo){
+        return res.status(404).send();
+      }
+
+      res.send(todo);
+    }).catch((e) => res.status(400).send());
 });
 
 module.exports = {
