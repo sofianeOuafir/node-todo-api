85d3cedfd4a18e9582bfc885b5a056de5309ad90
Sofiane OUAFIR <sofiane.ouafir@live.fr>
2018-07-26 16:57:01 +0100

    added tests for DELETE /users/me/token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -330,3 +330,21 @@ describe('POST /users/login', () => {
   });
 });
 
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', seedUsers[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if(err){
+          return done(err);
+        }
+
+        User.findById(seedUsers[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+})
